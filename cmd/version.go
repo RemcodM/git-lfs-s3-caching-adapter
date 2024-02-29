e7a5d748a7003693f00b94821d162e383e0c2af5
Remco de Man <gitlab@remcodeman.nl>
2024-02-29 07:35:26 +0000

    Version 0.6.0 based on Git LFS 3.5.0

--- cmd/version.go
+++ cmd/version.go
@@ -34,7 +34,7 @@ var versionCmd = &cobra.Command{
 	Short: "Returns the version of the Git LFS S3 caching adapter",
 	Long:  `Returns the version of the running Git LFS S3 caching adapter binary.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		version := "0.5.2"
+		version := "0.6.0"
 		if short {
 			fmt.Println(version)
 		} else {
